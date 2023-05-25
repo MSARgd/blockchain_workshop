36c73aeec769db34eb4f386fa437d7a0fd562f1c
sidiAbdalla <sidabdallard@gmail.com>
2023-05-25 23:00:22 +0100

    Thu May 25 11:00:22 PM +01 2023

---
+++ src/main/java/ma/enset/BlockchainWorkshop/BlockchainWorkshopApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.BlockchainWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainWorkshopApplication.class, args);
+	}
+
+}
